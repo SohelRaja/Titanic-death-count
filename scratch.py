ccb98d26f295d8667ac9f7e17aebf1da8dc9622f
Arghadip Chakraborty <38630062+arghac14@users.noreply.github.com>
2018-08-23 23:19:43 +0530

    Creating the scratch

---
+++ scratch.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+df=pd.read_csv("D:\\PROJECTS\\Python\\DrAIvex project 2(Titanic-Death-count)\\train.csv")
+
+mean=df['Age'].mean()
+
+df['Age'].fillna(mean,inplace=True)
+
+
+Pclass=pd.get_dummies(df['Pclass'])
+Sex=pd.get_dummies(df['Sex'])
+Age=pd.get_dummies(df['Age'])
+
+df=pd.concat([df,Pclass,Sex,Age],axis=1)
+df.drop(['PassengerId','Sex','Age','Pclass','Name','Ticket','Cabin','SibSp','Parch','Fare','Embarked'],axis=1,inplace=True)
+
+
+
+x=df.drop('Survived',axis=1)
+y=pd.read_csv("D:\\PROJECTS\\Python\\DrAIvex project 2(Titanic-Death-count)\\test_y.csv")
+
+x_train,x_test,y_train,y_test=train_test_split(x, y, test_size=0.2, random_state=0)
+reg=LogisticRegression()
+reg.fit(x_train,y_train)
+pred=reg.predict(x_test)
+print(pred)
+print(accuracy_score(y_test,pred))
