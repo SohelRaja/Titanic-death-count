68b08e3f93c1060b789f7a3363cb134fa8567a7e
Arghadip Chakraborty <chakrabortyarghadip5@gmail.com>
2018-09-11 00:00:54 +0530

    Update Titanic.py

--- Titanic.py
+++ Titanic.py
@@ -4,7 +4,7 @@ import seaborn as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
-df=pd.read_csv("D:\\PROJECTS\\Python\\DrAIvex project 2(Titanic-Death-count)\\train.csv")
+df=pd.read_csv("train.csv")
 
 mean=df['Age'].mean()
 
@@ -21,7 +21,7 @@ df.drop(['PassengerId','Sex','Age','Pclass','Name','Ticket','Cabin','SibSp','Par
 
 
 x=df.drop('Survived',axis=1)
-y=pd.read_csv("D:\\PROJECTS\\Python\\DrAIvex project 2(Titanic-Death-count)\\test_Y.csv")
+y=df['Survived']
 
 x_train,x_test,y_train,y_test=train_test_split(x, y, test_size=0.2, random_state=0)
 reg=LogisticRegression()
